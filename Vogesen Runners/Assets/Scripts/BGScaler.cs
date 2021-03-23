23d794bd880ed0c03b1cfd67c07570795261d3b9
Provinix <nguyenkevin9606icloud.com@gmail.com>
2021-03-23 21:38:16 +0100

    Ground Fix 2.0

--- Vogesen Runners/Assets/Scripts/BGScaler.cs
+++ Vogesen Runners/Assets/Scripts/BGScaler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BGScaler : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
@@ -10,18 +11,15 @@ public class BGScaler : MonoBehaviour {
         // Breite berechnen
         float width = height * Screen.width / Screen.height;
 
-
         if (gameObject.name == "Background") {
             //GameObject (Background) skalieren
-            transform.localScale = new Vector3(width, height, 0);
-
-        } 
-        else
-            transform.localScale = new Vector3(width, 2, 0);
-        
+            transform.localScale = new Vector3 (width, height, 0);
+        } else
+            transform.localScale = new Vector3 (width, 2, 0);      
     }
 
     // Update is called once per frame
     void Update() {
+    
     }
 }
