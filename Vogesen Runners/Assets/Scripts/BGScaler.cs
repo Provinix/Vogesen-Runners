69aca5e24f472a1edcdf3b259b0da85f1ee18d92
Provinix <nguyenkevin9606icloud.com@gmail.com>
2021-03-23 19:19:44 +0100

    Fore Ground

--- Vogesen Runners/Assets/Scripts/BGScaler.cs
+++ Vogesen Runners/Assets/Scripts/BGScaler.cs
@@ -2,33 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGScaler : MonoBehaviour
-{
+public class BGScaler : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         // Höhe berechnen
         float height = Camera.main.orthographicSize * 2f;
         // Breite berechnen
         float width = height * Screen.width / Screen.height;
+
+
+        if (gameObject.name == "Background") {
+            //GameObject (Background) skalieren
+            transform.localScale = new Vector3(width, height, 0);
+
+        } 
+        else
+            transform.localScale = new Vector3(width, 2, 0);
         
-
-        if(gameObject.name == "Background") { 
-           //GameObject (Background) skalieren
-           transform.localScale = new Vector3(width, height, 0);
     }
-    else
-    transform.localScale = new Vector3(width, 2, 0);
-        
-    }
-
-
-             
-}
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 }
