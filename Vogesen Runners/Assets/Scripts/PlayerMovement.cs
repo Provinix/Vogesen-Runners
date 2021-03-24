db27a0c6a30eb65f80f6e161d49f2cd4d34a0136
Provinix <nguyenkevin9606icloud.com@gmail.com>
2021-03-24 17:15:26 +0100

    Player Movement Jump

---
+++ Vogesen Runners/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float jumpForce = 5f;
+
+    private Rigidbody2D myRB;
+    // Start is called before the first frame update
+    void Start() {
+        myRB = GetComponent<Rigidbody2D> ();
+    }
+
+    // Jump Funktion
+    public void Jump()
+    {
+        myRB.velocity = new Vector2(0f, jumpForce);
+    }
+
+
+
+        
+}
