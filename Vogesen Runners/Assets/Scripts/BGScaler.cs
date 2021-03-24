8334b23268102cd30978d4fec97f716fd8cea4fd
Provinix <nguyenkevin9606icloud.com@gmail.com>
2021-03-24 09:40:00 +0100

    Background fixed 3.0

--- Vogesen Runners/Assets/Scripts/BGScaler.cs
+++ Vogesen Runners/Assets/Scripts/BGScaler.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class BGScaler : MonoBehaviour {
     // Start is called before the first frame update
+   
     void Start() {
         // Höhe berechnen
         float height = Camera.main.orthographicSize * 2f;
