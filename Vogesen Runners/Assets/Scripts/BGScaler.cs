473ee7f02bc63d4d466734ff1df7913bfa63abb3
seuseli <69416922+seuseli@users.noreply.github.com>
2021-03-24 17:07:20 +0100

    Lobby to Game fix

--- Vogesen Runners/Assets/Scripts/BGScaler.cs
+++ Vogesen Runners/Assets/Scripts/BGScaler.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class BGScaler : MonoBehaviour {
     // Start is called before the first frame update
+   
     void Start() {
         // Höhe berechnen
         float height = Camera.main.orthographicSize * 2f;
         // Breite berechnen
         float width = height * Screen.width / Screen.height;
 
-
         if (gameObject.name == "Background") {
             //GameObject (Background) skalieren
-            transform.localScale = new Vector3(width, height, 0);
-
-        } 
-        else
-            transform.localScale = new Vector3(width, 2, 0);
-        
+            transform.localScale = new Vector3 (width, height, 0);
+        } else
+            transform.localScale = new Vector3 (width, 2, 0);      
     }
 
     // Update is called once per frame
     void Update() {
+    
     }
 }
